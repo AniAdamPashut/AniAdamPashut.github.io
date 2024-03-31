f348feaaa53cf9586bf57545801c18476acb1bb9
benami <minebenking@gmail.com>
2024-04-01 00:23:45 +0300

    wtf is a python script?!

--- script.py
+++ script.py
@@ -15,9 +15,39 @@ with open(r'.\base.html') as f:
 for post in posts:
     os.remove(f'{posts_folder}\\{post}')
 
+index_updating = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>benAmi spews stuff</title>
+    <link rel="stylesheet" href="/styles.css">
+</head>
+<body>
+    <article>
+        <h1>The shitposting website benami wrote for suffering</h1>
+        <h3>Posts</h3>
+        <ul id="posts">
+            <LIST_GOES_HERE/>
+        </ul>
+    </article>
+</body>
+</html>"""
+
+post_list = []
+
 for file in files:
     html = pypandoc.convert_file(f'{md_folder}\\{file}', 'html', extra_args=['--wrap=none'])
     full_content = base.replace("<CONTENT_GOES_IN_HERE/>", html)
 
-    with open(f'{posts_folder}\\{file[:-3]}.html', 'w', encoding='utf-8') as f:
+    out = f'{posts_folder}\\{file[:-3]}.html'
+    with open(out, 'w', encoding='utf-8') as f:
         f.write(full_content.encode('utf-8').replace(b'\r\n', b'\n').decode())
+
+    list_item = f'<li><a href="{out}">{file[:-3]}</a></li>'
+    post_list.append(list_item)
+
+index_file = index_updating.replace('<LIST_GOES_HERE/>', '\n'.join(post_list))
+with open('index.html', 'w') as f:
+    f.write(index_file)
